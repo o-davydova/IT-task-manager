242d9d4984e61260a87a694dbcffeb1d0651c0d6
Davydova Olha <o.chookity@gmail.com>
2023-10-03 14:01:09 +0300

    Add `TaskType` and `Task` models

--- task_hub/models.py
+++ task_hub/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    LOW = 'L'
+    MEDIUM = 'M'
+    HIGH = 'H'
+    URGENT = 'U'
+
+    PRIORITY_CHOICES = [
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium'),
+        (HIGH, 'High'),
+        (URGENT, 'Urgent'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(
+        max_length=6,
+        choices=PRIORITY_CHOICES,
+        default=MEDIUM,
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+
+    def __str__(self):
+        return f"{self.name} - {self.get_priority_display()}"
