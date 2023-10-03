fd65ba19489a6b0d6b12fc9e28ac92950059fb92
Davydova Olha <o.chookity@gmail.com>
2023-10-03 14:04:30 +0300

    Configure custom user model and login redirect URL

--- IT_task_manager/settings.py
+++ IT_task_manager/settings.py
@@ -100,6 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "task_hub.Worker"
+
+LOGIN_REDIRECT_URL = "/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
