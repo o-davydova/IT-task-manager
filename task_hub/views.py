49215fb98531f92c461ca20fe1369cbf2cf28247
Davydova Olha <o.chookity@gmail.com>
2023-10-07 12:47:43 +0300

    Create custom Logout View with redirect to Login

--- task_hub/views.py
+++ task_hub/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from task_hub.forms import UserLoginForm
+from task_hub.models import Worker, Task, TaskType, Position
+
+
+class IndexView(LoginRequiredMixin, generic.TemplateView):
+    template_name = "task_hub/index.html"
+
+
+class CustomLogoutView(LogoutView):
+    next_page = '/accounts/login'
