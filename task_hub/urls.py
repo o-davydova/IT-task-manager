ddeee371f31963b8501351f3cdd2b69fb560ec89
Davydova Olha <o.chookity@gmail.com>
2023-10-10 21:29:07 +0300

    Add `Task delete` page URL to urlpatterns

--- task_hub/urls.py
+++ task_hub/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 
-from task_hub.views import TaskListView, IndexView, TaskDetailView, TaskUpdateView
+from task_hub.views import (
+    TaskListView,
+    IndexView,
+    TaskDetailView,
+    TaskUpdateView,
+    TaskDeleteView
+)
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 app_name = "task-hub"
