a3b774eab1f659d4d126510ff21a6b7e588e3c77
Davydova Olha <o.chookity@gmail.com>
2023-10-10 19:45:25 +0300

    Add `Task update` page URL to urlpatterns

--- task_hub/urls.py
+++ task_hub/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from task_hub.views import TaskListView, IndexView, TaskDetailView
+from task_hub.views import TaskListView, IndexView, TaskDetailView, TaskUpdateView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
 ]
 
 app_name = "task-hub"
