db6b2599fcb773e6bf07529683283cf197d68166
Davydova Olha <o.chookity@gmail.com>
2023-10-10 12:27:45 +0300

    Update `Task Detail View` with assignee count

--- task_hub/views.py
+++ task_hub/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.views import LogoutView
 from django.shortcuts import redirect, render
 from django.views import generic
 
-from task_hub.models import Task
+from task_hub.models import Task, Worker
 
 
 class IndexView(generic.View):
@@ -20,6 +20,13 @@ class TaskListView(LoginRequiredMixin, generic.ListView):
 
 class TaskDetailView(LoginRequiredMixin, generic.DetailView):
     model = Task
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskDetailView, self).get_context_data(**kwargs)
+        num_assignee = self.object.assignees.count()
+        context["num_assignee"] = num_assignee
+
+        return context
 
 
 class CustomLogoutView(LogoutView):
