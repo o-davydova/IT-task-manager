140bb5b580e290e39601e7e83adbcd466ce56244
Davydova Olha <o.chookity@gmail.com>
2023-10-11 18:27:01 +0300

    Add `Task create` page URL to urlpatterns

--- task_hub/urls.py
+++ task_hub/urls.py
@@ -5,12 +5,14 @@ from task_hub.views import (
     IndexView,
     TaskDetailView,
     TaskUpdateView,
-    TaskDeleteView
+    TaskDeleteView,
+    TaskCreateView
 )
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
