f162d9888a5460e97d0be5e3f309e5a559e1ca37
Davydova Olha <o.chookity@gmail.com>
2023-10-18 20:26:52 +0300

    Add `Position create` page URL to urlpatterns

--- task_hub/urls.py
+++ task_hub/urls.py
@@ -18,7 +18,7 @@ from task_hub.views import (
     WorkerUpdateView,
     PositionListView,
     PositionDetailView,
-    PositionUpdateView,
+    PositionUpdateView, PositionCreateView,
 )
 
 urlpatterns = [
@@ -28,16 +28,20 @@ urlpatterns = [
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+
     path("task-types/", TaskTypeListView.as_view(), name="task-type-list"),
     path("task-types/create/", TaskTypeCreateView.as_view(), name="task-type-create"),
     path("task-types/<int:pk>/", TaskTypeDetailView.as_view(), name="task-type-detail"),
     path("task-types/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="task-type-update"),
     path("task-types/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="task-type-delete"),
+
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
     path("workers/<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete"),
     path("workers/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
+
     path("positions/", PositionListView.as_view(), name="position-list"),
+    path("positions/create", PositionCreateView.as_view(), name="position-create"),
     path("positions/<int:pk>/", PositionDetailView.as_view(), name="position-detail"),
     path("positions/<int:pk>/update/", PositionUpdateView.as_view(), name="position-update"),
 ]
