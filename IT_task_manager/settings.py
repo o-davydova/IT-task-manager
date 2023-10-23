3d844f643b8d411e4d89f150436321b358c55b49
Davydova Olha <o.chookity@gmail.com>
2023-10-23 15:12:58 +0300

    Update SECRET_KEY and DEBUG in settings.py

--- IT_task_manager/settings.py
+++ IT_task_manager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4y1^3=z+l3o@3r7&bhpjqo4kqo3z6_72x6p-lu-mgnt#y7mshe"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-4y1^3=z+l3o@3r7&bhpjqo4kqo3z6_72x6p-lu-mgnt#y7mshe",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ]
 
 
 # Application definition
